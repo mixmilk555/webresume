7dcc5ace154f0e4ded0b26ed93ef1af1b7895c39
mixmilk555 <mixmilk777@gmail.com>
2021-03-07 20:37:34 +0700

    fixed image and name

--- src/component/pages/mypicture.js
+++ src/component/pages/mypicture.js
@@ -5,9 +5,9 @@ export default function MyPicture() {
     return (
         <div id="mypicture" className="heroBlock">
             <div className="container-fluid" style={{ height: '100%' }}>
-                <div style={{ width: '40%', margin: "0 auto" }}>
-                    <img src="https://i.ibb.co/yXZzp3j/3.jpg" alt="myPicture" style={{ width: '100%', height: 'auto',borderRadius:'10%' }} />
-                    <h2 data-text="Makawan&nbsp;Maneephong">Makawan&nbsp;Maneephong</h2>
+                <div style={{ width: '50%', margin: "0 auto" }}>
+                    <img src="https://i.ibb.co/yXZzp3j/3.jpg" alt="myPicture" style={{marginTop:'15%', width: '100%', height: 'auto', borderRadius: '10%' }} />
+                    <p style={{ textAlign:'center',width: 'auto', wordWrap: 'break-word' }} data-text="Makawan&nbsp;&nbsp;&nbsp;Maneephong">Makawan&nbsp;&nbsp;&nbsp;Maneephong</p>
                 </div>
             </div>
         </div>
